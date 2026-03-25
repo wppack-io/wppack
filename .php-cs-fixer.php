acb2a2e1d16f1682732efb82eb3ba25273ed0516
TSURU <tsuyoshi.tsurushima@gmail.com>
2026-03-25 18:30:47 +0900

    chore: add license header to all source files
    - Add header_comment rule to .php-cs-fixer.php with MIT license block
    - Add notPath exclusions for generated/template files
    - Apply license header to all PHP files under src/ and tests/

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -6,7 +6,10 @@ $finder = (new PhpCsFixer\Finder())
     ->in([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
+    ])
+    ->notPath('templates/')
+    ->notPath('Debug/src/Toolbar/Panel/ToolbarIcons.php')
+    ->notPath('Mime/src/MimeTypeMap.php');
 
 return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
@@ -14,5 +17,18 @@ return (new PhpCsFixer\Config())
         '@PER-CS2.0' => true,
         '@PER-CS2.0:risky' => true,
         'declare_strict_types' => true,
+        'header_comment' => [
+            'header' => <<<'EOF'
+This file is part of the WpPack package.
+
+(c) Tsuyoshi Tsurushima
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+EOF,
+            'comment_type' => 'comment',
+            'location' => 'after_open',
+            'separate' => 'bottom',
+        ],
     ])
     ->setFinder($finder);
--- src/Component/Debug/drop-in/fatal-error-handler.php
+++ src/Component/Debug/drop-in/fatal-error-handler.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the WpPack package.
+ *
+ * (c) Tsuyoshi Tsurushima
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 /**
  * WpPack Fatal Error Handler Drop-in
